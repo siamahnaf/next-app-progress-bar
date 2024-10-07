b73511e048c710b948fbf31a7041af45305f0e24
Siam Ahnaf <siamahnaf198@gmail.com>
2024-10-07 13:37:37 +0600

    first commit

---
+++ dist/esm/components/pages.d.ts
@@ -0,0 +1,2 @@
+import { NextTopLoaderProps } from './app';
+export declare const PageProgressBar: ({ color: propColor, height: propHeight, showSpinner, crawl, crawlSpeed, initialPosition, easing, speed, shadow, delay, template, zIndex, showAtBottom, }: NextTopLoaderProps) => JSX.Element;
